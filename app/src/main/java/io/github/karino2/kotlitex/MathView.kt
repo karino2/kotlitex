ef1272fd8e9f5efbd3d71bcb659c0709772279b7
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-01-15 21:07:36 -0800

    Render `x^2` simply

--- app/src/main/java/io/github/karino2/kotlitex/MathView.kt
+++ app/src/main/java/io/github/karino2/kotlitex/MathView.kt
@@ -15,50 +15,7 @@ class MathView(context: Context, attrSet: AttributeSet) : View(context, attrSet)
         val parsed =  parser.parse()
         nodes = RenderTreeBuilder.buildExpression(parsed, options, true)
     }
-    /*
-    val nodes = list {
-        span("mord", children=list {
-            sym("x", "mord mathdefault", height=0.43056, skew=0.02778, width=0.57153, maxFontSize=1.0)
-            span("msupsub", height=0.8141079999999999, maxFontSize = 0.7,children=list {
-                span("vlist-t", height=0.8141079999999999, depth=-0.367, maxFontSize = 0.7, children=list {
-                    span("vlist-r", height=0.45110799999999995, maxFontSize = 0.7,  children=list {
-                        span(
-                            "vlist",
-                            height = 0.45110799999999995,
-                            sheight = "0.8141079999999999em",
-                            maxFontSize = 0.7,
-                            children = list {
-                                span(
-                                    "",
-                                    top = "-3.063em",
-                                    height = 0.45110799999999995,
-                                    italic = 0.05,
-                                    maxFontSize = 0.7,
-                                    children = list {
-                                        span("pstruct", sheight = "2.7em")
-                                        span(
-                                            "sizing reset-size6 size3 mtight",
-                                            height = 0.45110799999999995,
-                                            maxFontSize = 0.7,
-                                            children = list {
-                                                sym(
-                                                    "2",
-                                                    "mord mtight",
-                                                    height = 0.64444,
-                                                    width = 0.5,
-                                                    maxFontSize = 0.7,
-                                                    skew = 0.0
-                                                )
-                                            })
-                                    })
-                            })
-                    })
-                })
-
-            })
-        })
-    }
-    */
+    var point: PointF = PointF(0f, 0f)
 
     val BaseTextSize: Double = 50.0;
 
@@ -70,21 +27,26 @@ class MathView(context: Context, attrSet: AttributeSet) : View(context, attrSet)
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        for (renderNode in nodes) {
-            drawNode(canvas, renderNode)
-        }
+        // The way it keeps the current cursor here doesn't look right. Maybe introducing Renderer later?
+        point.x = 100f
+        point.y = 100f
+
+        drawRenderNodes(canvas, nodes)
     }
 
-    private fun drawNode(canvas: Canvas, parent: RenderNode) {
-        when (parent) {
-            is SpanNode -> {
-                for (node in parent.children) {
-                    drawNode(canvas, node)
+    private fun drawRenderNodes(canvas: Canvas, nodes: List<RenderNode>) {
+        for (parent in nodes) {
+            when (parent) {
+                is SpanNode -> {
+                    drawRenderNodes(canvas, parent.children)
                 }
-            }
-            is SymbolNode -> {
-                textPaint.textSize = (BaseTextSize * parent.maxFontSize).toFloat()
-                canvas.drawText(parent.text, 100.0f, 100.0f, textPaint)
+                is SymbolNode -> {
+                    textPaint.textSize = (BaseTextSize * parent.maxFontSize).toFloat()
+                    canvas.drawText(parent.text, point.x, point.y, textPaint)
+
+                    // Assuming that the whole rendering process is Left-to-Right, which would not be true
+                    point.x += (BaseTextSize * parent.width).toFloat()
+                }
             }
         }
     }
