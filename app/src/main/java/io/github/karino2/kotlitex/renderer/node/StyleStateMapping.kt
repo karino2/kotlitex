d717082711757ceda0cefdc20bf5220d226325f3
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2019-01-29 22:56:35 -0800

    Implement StyleStateMapping
    Handling these attributes seems enough to be canvas-latex compatible.

--- app/src/main/java/io/github/karino2/kotlitex/renderer/node/StyleStateMapping.kt
+++ app/src/main/java/io/github/karino2/kotlitex/renderer/node/StyleStateMapping.kt
@@ -4,11 +4,48 @@ import io.github.karino2.kotlitex.RenderNode
 import io.github.karino2.kotlitex.renderer.RenderingState
 
 object StyleStateMapping {
-    fun createState(state: RenderingState, node: RenderNode): RenderingState {
-        val top = node.style.top?.replace("em", "")?.toDouble() ?: return state
-        println(state.fontSize())
-        println(state.size)
-        println(top)
-        return state.copy(y = state.y + (top * state.fontSize()))
+    fun createState(original: RenderingState, node: RenderNode): RenderingState {
+        var state = original.copy()
+
+        val color = node.style.color
+        if (color != null) {
+            state = state.copy(color = color)
+        }
+
+        val top = node.style.top
+        if (top != null) {
+            state = state.copy(y = state.y + parseEm(top, state.fontSize()))
+        }
+
+        val paddingLeft = node.style.paddingLeft
+        if (paddingLeft != null) {
+            val spacingLeft = parseEm(paddingLeft, state.fontSize())
+            val padLeftNode = HPaddingNode(state.klasses)
+            padLeftNode.setPosition(state.nextX(), state.y)
+            padLeftNode.bounds.width = spacingLeft
+            state.vlist.addCell(padLeftNode)
+            state = state.withResetMargin()
+        }
+
+        val marginLeft = node.style.marginLeft
+        if (marginLeft != null) {
+            state = state.copy(marginLeft = parseEm(marginLeft, state.fontSize()))
+        }
+
+        val marginRight = node.style.marginRight
+        if (marginRight != null) {
+            state = state.copy(marginRight = parseEm(marginRight, state.fontSize()))
+        }
+
+        val minWidth = node.style.minWidth
+        if (minWidth != null) {
+            state = state.copy(minWidth = parseEm(minWidth, state.fontSize()))
+        }
+
+        return state
+    }
+
+    private fun parseEm(str: String, fontSize: Double): Double {
+        return str.replace("em", "").toDouble() * fontSize
     }
 }
