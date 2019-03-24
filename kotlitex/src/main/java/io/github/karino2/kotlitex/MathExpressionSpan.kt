e93e429f70f21a4a3481196ae8c8c065051cef0a
Kazuma Arino <hogeika2@gmail.com>
2019-03-24 13:49:58 +0900

    Now render to specified size. But intrinsic size is based on hard coded size.

--- kotlitex/src/main/java/io/github/karino2/kotlitex/MathExpressionSpan.kt
+++ kotlitex/src/main/java/io/github/karino2/kotlitex/MathExpressionSpan.kt
@@ -4,11 +4,14 @@ import android.content.res.AssetManager
 import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.text.TextPaint
-import android.text.style.DynamicDrawableSpan
+import android.text.style.ReplacementSpan
+import android.util.Log
 import io.github.karino2.kotlitex.renderer.AndroidFontLoader
 import io.github.karino2.kotlitex.renderer.FontLoader
 import io.github.karino2.kotlitex.renderer.VirtualNodeBuilder
 import io.github.karino2.kotlitex.renderer.node.*
+import java.lang.ref.WeakReference
+import kotlin.math.roundToInt
 
 
 private class MathExpressionDrawable(expr: String, baseSize: Float, val fontLoader: FontLoader, val drawBounds: Boolean = false) : Drawable() {
@@ -37,7 +40,7 @@ private class MathExpressionDrawable(expr: String, baseSize: Float, val fontLoad
     }
 
     override fun draw(canvas: Canvas) {
-        drawRenderNodes(canvas, rootNode)
+        drawRenderNodes(canvas, rootNode, 1.0f)
     }
 
     // TODO
@@ -50,51 +53,52 @@ private class MathExpressionDrawable(expr: String, baseSize: Float, val fontLoad
         return y.toFloat() + 200
     }
 
-    private fun drawBounds(canvas: Canvas, bounds: Bounds) {
+    private fun drawBounds(canvas: Canvas, bounds: Bounds, ratio: Float) {
         if (! drawBounds) {
             return
         }
 
-        val x = translateX(bounds.x)
-        val y = translateY(bounds.y)
+        val x = translateX(bounds.x)*ratio
+        val y = translateY(bounds.y)*ratio
 
         paint.color = Color.RED
         paint.strokeWidth = 1.0f
         paint.style = Paint.Style.STROKE
 
         // TODO: This doesn't look right though...
-        canvas.drawRect(RectF(x, y - bounds.height.toFloat(), x + bounds.width.toFloat(), y), paint)
+        // canvas.drawRect(RectF(x, y - bounds.height.toFloat()*ratio, x + bounds.width.toFloat()*ratio, y), paint)
+        canvas.drawRect(RectF(x, y - bounds.height.toFloat()*ratio, x + bounds.width.toFloat()*ratio, y + bounds.height.toFloat()*ratio), paint)
     }
 
-    private fun drawRenderNodes(canvas: Canvas, parent: VirtualCanvasNode) {
+    private fun drawRenderNodes(canvas: Canvas, parent: VirtualCanvasNode, ratio: Float) {
         when (parent) {
             is VirtualContainerNode<*> -> {
                 parent.nodes.forEach {
-                    drawRenderNodes(canvas, it)
+                    drawRenderNodes(canvas, it, ratio)
                 }
             }
             is TextNode -> {
                 // TODO: temp font test.
                 textPaint.typeface = fontLoader.toTypeface(parent.font)
-                textPaint.textSize = parent.font.size.toFloat()
-                val x = translateX(parent.bounds.x)
-                val y = translateY(parent.bounds.y)
+                textPaint.textSize = parent.font.size.toFloat()*ratio
+                val x = translateX(parent.bounds.x)*ratio
+                val y = translateY(parent.bounds.y)*ratio
                 canvas.drawText(parent.text, x, y, textPaint)
-                drawBounds(canvas, parent.bounds)
+                drawBounds(canvas, parent.bounds, ratio)
             }
             is HorizontalLineNode -> {
                 paint.color = Color.BLACK
-                paint.strokeWidth = parent.bounds.height.toFloat()
-                val x = translateX(parent.bounds.x)
-                val y = translateY(parent.bounds.y)
-                canvas.drawLine(x, y, x + parent.bounds.width.toFloat(), y, paint)
-                drawBounds(canvas, parent.bounds)
+                paint.strokeWidth = parent.bounds.height.toFloat()*ratio
+                val x = translateX(parent.bounds.x)*ratio
+                val y = translateY(parent.bounds.y)*ratio
+                canvas.drawLine(x, y, x + parent.bounds.width.toFloat()*ratio, y, paint)
+                drawBounds(canvas, parent.bounds, ratio)
             }
             is PathNode -> {
-                val x = translateX(parent.bounds.x)
-                val y = (translateY(parent.bounds.y) - parent.bounds.height).toFloat()
+                val x = translateX(parent.bounds.x)*ratio
+                val y = (translateY(parent.bounds.y) - parent.bounds.height).toFloat()*ratio
 
-                drawBounds(canvas, parent.bounds)
+                drawBounds(canvas, parent.bounds, ratio)
 
 
                 // TODO: support other preserve aspect ratio.
@@ -102,7 +106,9 @@ private class MathExpressionDrawable(expr: String, baseSize: Float, val fontLoad
                 val mat = Matrix()
 
                 val heightvb = parent.rnode.viewBox.height
-                val (_, _, wb, hb) = parent.bounds
+                val (_, _, wbVirtual, hbVirtual) = parent.bounds
+                val wb = wbVirtual*ratio
+                val hb = hbVirtual*ratio
 
                 // this is preserveAspectRatio = meet
                 /*
@@ -142,14 +148,112 @@ private class MathExpressionDrawable(expr: String, baseSize: Float, val fontLoad
         }
     }
 
+    fun drawWithRatio(canvas: Canvas, ratio: Float) {
+        drawRenderNodes(canvas, rootNode, ratio)
+    }
+
 }
 
-class MathExpressionSpan(val expr: String, val baseSize: Float = 44.0f, val assetManager: AssetManager) : DynamicDrawableSpan() {
-    override fun getDrawable(): Drawable {
+// Similar to DynamicDrawableSpan, but getSize is a little different.
+// I create super class of DynamicDrawableSpan because getCachedDrawable is private and we neet it.
+class MathExpressionSpan(val expr: String, val baseHeightSpecified: Float?, val assetManager: AssetManager) : ReplacementSpan() {
+    enum class Align {
+        Bottom, BaseLine
+    }
+
+    var verticalAlignment = Align.Bottom
+
+    override fun getSize(paint: Paint, text: CharSequence?, start: Int, end: Int, fm: Paint.FontMetricsInt?): Int {
+        val d = getCachedDrawable()
+        val rect = d.bounds
+
+        baseHeightSpecified?.let { targetHeight ->
+            val ratio = targetHeight/virtualBaseHeight
+            if(fm == null) {
+                return (rect.right*ratio).roundToInt()
+            }
+
+
+            val lineNum = rect.bottom/virtualBaseHeight
+            // We follow DynamicDrawableSpan logic. But it's a little different from no-size specified case.
+            fm.ascent = -(lineNum*targetHeight).roundToInt()
+            fm.descent = 0
+            fm.top = fm.ascent
+            fm.bottom = 0
+            // should we roundUp?
+            return (rect.right*ratio).roundToInt()
+        }
+        // below here is baseHeightSpecified == null case.
+
+        if(fm != null) {
+            // TODO: consider handling of baseline.
+            // I think we should align drawable's drawRenderNode baseline to fm.baseline.
+            // But our font is different, so it's not obvious whether my assumption is correct.
+            // Currently, I just use whole height as our box height.
+
+
+            // fm.bottom and top seems zero. There seems no way to know current line height.
+            /*
+            val lineHeight = fm.bottom - fm.top
+            // sometime, bottom and top is zero. in this case, we regard
+            val ratio = if(lineHeight == 0) 1.0 else lineHeight/ rect.bottom.toDouble()
+
+            // should we roundup?
+            return (rect.right*ratio).roundToInt()
+             */
+            fm.ascent = -rect.bottom
+            fm.descent = 0
+            fm.top = fm.ascent
+            fm.bottom
+        }
+
+        return rect.right
+    }
+
+    override fun draw(
+        canvas: Canvas,
+        text: CharSequence?,
+        start: Int,
+        end: Int,
+        x: Float,
+        top: Int,
+        y: Int,
+        bottom: Int,
+        paint: Paint
+    ) {
+        val b = getCachedDrawable()
+
+        // We always use full height. So we do not need any alignment, right?
+        val ratio = (bottom-top).toDouble() / b.bounds.bottom.toDouble()
+        Log.d("kotlitex", "ratio=$ratio")
+
+
+
+        canvas.save()
+        canvas.translate(x, top.toFloat())
+        b.drawWithRatio(canvas, ratio.toFloat())
+
+        canvas.restore()
+    }
+    val virtualBaseHeight = 100.0f
+
+    private fun getDrawable(): MathExpressionDrawable {
         // TODO: drawBounds should be always false. Unlike baseSize, we don't have to expose the flag to end-users.
-        val drawable = MathExpressionDrawable(expr, baseSize,
+        val drawable = MathExpressionDrawable(expr, virtualBaseHeight,
             AndroidFontLoader(assetManager), drawBounds = true)
-        drawable.setBounds(drawable.bounds.left, drawable.bounds.top, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.setBounds(drawable.bounds.left, drawable.bounds.top, drawable.intrinsicWidth, drawable.intrinsicHeight);
         return drawable
     }
+
+    private fun getCachedDrawable() : MathExpressionDrawable {
+        val wr = drawableRef
+        val d = wr?.get()
+        if (d != null)
+            return d
+        val newDrawable = getDrawable()
+        drawableRef = WeakReference(newDrawable)
+        return newDrawable
+    }
+
+    private var drawableRef : WeakReference<MathExpressionDrawable>? = null
 }
